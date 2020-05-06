dedf3cc72eccbbe8cee0efc9487f9dc597e11fc3
lolli <lpattori@gmail.com>
2020-05-06 15:47:43 -0300

    Split Functional and unit tests

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,18 @@
+import time
+
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+
+        # she notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        time.sleep(1.0)
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512, delta=20
+        )
